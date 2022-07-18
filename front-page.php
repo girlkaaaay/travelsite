15c1726831841c7d1d3338ce05282964da5fa055
Kristen Anderson <84354231+girlkaaaay@users.noreply.github.com>
2022-07-18 15:51:39 -0700

    added hero section, and added the icon section, along with all the styling

---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php  
+/**  
+ * Template Name: Homepage  
+ *  
+ * @package TravelSite  
+ *  
+ */  
+?>  
+<?php get_header(); ?>
+
+<!-- This is the start of hero section -->
+<main id="topBtn">
+        <div class="hero-text">
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </main>
+    <!-- This is the end of hero section --> 
+
+    <!-- This is the start of icon section -->
+    <section class="row">
+        <div class="column">
+            <img src="http://localhost:10078/wp-content/uploads/2022/07/globe.png">
+            <h2>CHOOSE YOUR DESTIANTION</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis inventore, perspiciatis rerum quisquam nam ex. </p>
+        </div>
+        <div class="column">
+            <img src="http://localhost:10078/wp-content/uploads/2022/07/airport.png" alt="pink globe icon.">
+            <h2>CATCH YOUR FLIGHT</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis inventore, perspiciatis rerum quisquam nam ex. </p>
+        </div>
+        <div class="column">
+            <img src="http://localhost:10078/wp-content/uploads/2022/07/pin.png" alt="pink globe icon.">
+            <h2>LAND IN PARADISE</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis inventore, perspiciatis rerum quisquam nam ex. </p>
+        </div>
+    </section>
+    <!-- This is the end of icon section -->
